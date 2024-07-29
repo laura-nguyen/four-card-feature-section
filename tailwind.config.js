d022ab8a0761be31479db5c2f6d1d57f802be982
Laura Nguyen <hello@lauranguyen.me>
2024-07-29 01:19:59 -0400

    added styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        hp: ".9375rem",
+        p: ".8125rem",
+      },
+    },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      // Primary
+      red: "hsl(0, 78%, 62%)",
+      cyan: "hsl(180, 62%, 55%)",
+      orange: "hsl(34, 97%, 64%)",
+      blue: "hsl(212, 86%, 64%)",
+
+      // Neutral
+      "very-dark-blue": "hsl(234, 12%, 34%)",
+      "grayish-blue": "hsl(229, 6%, 66%)",
+      "very-light-gray": "hsl(0, 0%, 98%)",
+    },
   },
   plugins: [],
 };
